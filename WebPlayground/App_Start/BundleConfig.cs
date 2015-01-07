544d2f95b04e3deb52fff7ff7f487c4e785e5b3c
jasonmorrison <jasonmorrison@fairfaxdatasystems.com>
2015-01-07 12:22:20 -0500

    Add iDea and wire components
    Added iDea theme and test index page to verify that all components are
    accessed correctly

--- WebPlayground/App_Start/BundleConfig.cs
+++ WebPlayground/App_Start/BundleConfig.cs
@@ -23,27 +23,50 @@ namespace WebPlayground
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/Custom/css").Include("~/Content/Custom/Site.css"));
+            bundles.Add(new StyleBundle("~/bundles/iDea/css").Include(
+                "~/Content/iDea/bootstrap/css/bootstrap-theme.css",
+                "~/Content/iDea/bootstrap/css/bootstrap.css",
+                "~/Content/iDea/fonts/font-awesome/css/font-awesome.css",
+                "~/Content/iDea/fonts/fontello/css/animation.css",
+                "~/Content/iDea/fonts/fontello/css/fontello-codes.css",
+                "~/Content/iDea/fonts/fontello/css/fontello-embedded.css",
+                "~/Content/iDea/fonts/fontello/css/fontello-ie7-codes.css",
+                "~/Content/iDea/fonts/fontello/css/fontello-ie7.css",
+                "~/Content/iDea/fonts/fontello/css/fontello.css",
+                "~/Content/iDea/animate.css",
+                "~/Content/iDea/animation.css",
+                "~/Content/iDea/custom.css",
+                "~/Content/iDea/style.css",
+                "~/Content/iDea/skins/light_blue.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new ScriptBundle("~/bundles/iDea/js").Include(
+                "~/Scripts/iDea/bootstrap/bootstrap.js",
+                "~/Scripts/iDea/custom.js",
+                "~/Scripts/iDea/template.js"));
 
-            bundles.Add(new StyleBundle("~/Content/Bootstrap/css").Include(                
-                "~/Content/Bootstrap/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/bundles/iDea/plugins/css").Include(
+                "~/Content/iDea/plugins/jquery.countdown/jquery.countdown.css",
+                "~/Content/iDea/plugins/magnific-popup/magnific-popup.css",
+                "~/Content/iDea/plugins/owl-carousel/owl.carousel.css",
+                "~/Content/iDea/plugins/owl-carousel/owl.theme.css",
+                "~/Content/iDea/plugins/owl-carousel/owl.tranistions.css",
+                "~/Content/iDea/plugins/rs-plugin/css/extralayers.css",
+                "~/Content/iDea/plugins/rs-plugin/css/settings-ie8.css",
+                "~/Content/iDea/plugins/rs-plugin/css/settings.css"));
 
-            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap/js").Include(
-                "~/Scripts/bootstrap/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/iDea/plugins/js").Include(
+                "~/Scripts/iDea/plugins/isotope/isotope.pkgd.js",
+                "~/Scripts/iDea/plugins/jquery.countdown/jquery.plugin.js",
+                "~/Scripts/iDea/plugins/jquery.countdown/jquery.countdown.js",
+                "~/Scripts/iDea/plugins/magnific-popup/jquery.magnific-popup.js",
+                "~/Scripts/iDea/plugins/owl-carousel/owl.carousel.js",                
+                //"~/Scripts/iDea/plugins/rs-plugin/jquery.themepunch.tools.min.js",
+                //"~/Scripts/iDea/plugins/rs-plugin/jquery.themepunch.revolution.js",                
+                "~/Scripts/iDea/plugins/jquery.appear.js",
+                "~/Scripts/iDea/plugins/jquery.countTo.js",
+                "~/Scripts/iDea/plugins/jquery.knob.min.js",
+                "~/Scripts/iDea/plugins/jquery.parallax-1.1.3.js",
+                "~/Scripts/iDea/plugins/jquery.sharrre.js"));
         }
     }
 }
